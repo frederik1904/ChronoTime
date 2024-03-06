dfa70c505b6d75c46cffd30102b07c79863bea74
Frederik Jacobsen <Frederik1904@gmail.com>
2024-03-06 19:55:15 +0100

    Added migration files

---
+++ DatabaseMigrationHandler/Migrations/20240306182956_AddedInitialTimeregistrationsModels.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseMigrationHandler.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedInitialTimeregistrationsModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Topic",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DisplayName = table.Column<string>(type: "text", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Changed = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Discriminator = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topic", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeRegistrations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TopicId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Changed = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ValidFrom = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ValidTo = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeRegistrations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TimeRegistrations_Topic_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topic",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeRegistrations_TopicId",
+                table: "TimeRegistrations",
+                column: "TopicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeRegistrations");
+
+            migrationBuilder.DropTable(
+                name: "Topic");
+        }
+    }
+}
---
+++ DatabaseMigrationHandler/Migrations/20240306184325_AddedUserToTimeRegistration.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseMigrationHandler.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserToTimeRegistration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "AUser");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "TimeRegistrations",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AUser",
+                type: "character varying(5)",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AUser",
+                table: "AUser",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeRegistrations_UserId",
+                table: "TimeRegistrations",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TimeRegistrations_AUser_UserId",
+                table: "TimeRegistrations",
+                column: "UserId",
+                principalTable: "AUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TimeRegistrations_AUser_UserId",
+                table: "TimeRegistrations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TimeRegistrations_UserId",
+                table: "TimeRegistrations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AUser",
+                table: "AUser");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TimeRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AUser");
+
+            migrationBuilder.RenameTable(
+                name: "AUser",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
